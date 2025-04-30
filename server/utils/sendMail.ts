d3e974aaabd061ab9c728213ded317b7cb092bd8
sahoonishi <nishisahoo293@gmail.com>
2025-04-30 23:54:59 +0530

    sending mail code

--- server/utils/sendMail.ts
+++ server/utils/sendMail.ts
@@ -8,3 +8,30 @@ interface EmailOptions {
   template:string;
   data:{[key:string]:any};
 }
+
+const sendMail = ()=>{
+  
+  // const transporter: Transporter = nodemailer.createTransport({
+  //   host: process.env.SMTP_HOST,
+  //   port: Number(process.env.SMTP_PORT),
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.SMTP_USER,
+  //     pass: process.env.SMTP_PASS,
+  //   },
+  // });
+
+  // return async (options: EmailOptions) => {
+  //   const { email, subject, template, data } = options;
+  //   const html = await ejs.renderFile(path.join(__dirname, `../mails/${template}.ejs`), data);
+    
+  //   const mailOptions = {
+  //     from: process.env.SMTP_USER,
+  //     to: email,
+  //     subject,
+  //     html,
+  //   };
+
+  //   await transporter.sendMail(mailOptions);
+  // };
+}
