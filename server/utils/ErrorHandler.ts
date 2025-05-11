aa57b582841f2c2eed0fb46f4deb6cd83c88d823
sahoonishi <nishisahoo293@gmail.com>
2025-05-11 11:05:31 +0530

    error handler updated

--- server/utils/ErrorHandler.ts
+++ server/utils/ErrorHandler.ts
@@ -1,9 +1,17 @@
-class ErrorHandler extends Error{
-  statusCode:Number;
-  constructor (message:any , statusCode:Number){
+class ErrorHandler extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number) {
     super(message);
     this.statusCode = statusCode;
-    Error.captureStackTrace(this , this.constructor);
+
+    //? Maintains proper stack trace (only available in V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
+
+    this.name = this.constructor.name;
   }
 }
+
 export default ErrorHandler;
