5491b3ea33b0896afae6c11a1aa8b020c4fcb2c8
sahoonishi <nishisahoo293@gmail.com>
2025-09-15 19:10:03 +0530

    layout model done

--- server/models/layout.model.ts
+++ server/models/layout.model.ts
@@ -1,1 +1,48 @@
-import {Schema,Document,Model} from "mongoose";
+import {Schema,Document,model} from "mongoose";
+
+export interface IFaq extends Document{
+question:string;
+answer:string;
+}
+export interface ICategory extends Document{
+    title:string;
+}
+export interface IBannerImage extends Document{
+    public_id:string;
+    url:string;
+}
+export interface ILayout extends Document{
+    type:string;
+    faq:IFaq[];
+    categories:ICategory[];
+    banner:{
+        image:IBannerImage;
+        title:string;
+        subtitle:string;
+    }
+}
+const faqSchema = new Schema<IFaq>({
+    question:{type:String},
+    answer:{type:String},
+});
+
+const categorySchema = new Schema<ICategory>({
+    title:{type:String}
+});
+
+const bannerimageSchema = new Schema<IBannerImage>({
+    public_id:{type:String},
+    url:{type:String},
+})
+const layoutSchema = new Schema<ILayout>({
+    type:{type:String},
+    faq:[faqSchema],
+    categories:[categorySchema],
+    banner:{
+        image:bannerimageSchema,
+        title:{type:String},
+        subtitle:{type:String},
+    },
+});
+const LayoutModel = model<ILayout>('Layout',layoutSchema);
+export default LayoutModel;
