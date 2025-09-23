5bf91299dd4d16592d541ed2e5e5f0a2cdcd3de8
sahoonishi <nishisahoo293@gmail.com>
2025-09-23 12:21:05 +0530

    overall UI setup

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="text-9xl bg-bg text-primary">
-      Hi
-    </div>
-  );
+"use client"
+import React , {FC,useState} from 'react'
+import Heading from './utils/Heading';
+interface props{
+  
 }
+const Page:FC<props> =(props)=> {
+  return (
+    <div>
+      <Heading
+      title='Sass'
+      description='Hey gyus what about today , uhh?'
+      keywords='coding,lms,udemy,learningmanagement'
+      />
+    </div>
+  )
+}
+
+export default Page;
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,17 +1,59 @@
-/** @type {import('tailwindcss').Config} */
-// Removed invalid @config line
-export default {
+// /** @type {import('tailwindcss').Config} */
+
+// import { Josefin_Sans } from "next/font/google";
+
+// // Removed invalid @config line
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily:{
+//         Poppins:["var(--font-Poppins)"],
+//         Josefin:["var(--font-Josefin)"],
+//       },
+//       colors: {
+//         primary: '#000000',
+//         bg:'#ffffff'
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
-      colors: {
-        primary: '#000000',
-        bg:'#ffffff'
+      fontFamily: {
+        Poppins: ["var(--font-Poppins)"],
+        Josefin: ["var(--font-Josefin)"],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      screens: {
+        "1000px": "1000px",
+        "1100px": "1100px",
+        "1200px": "1200px",
+        "1300px": "1300px",
+        "1500px": "1500px",
+        "800px": "800px",
+        "400px": "400px",
       },
     },
   },
   plugins: [],
-}
+};
+export default config;
